b536337b4fa725d3dfd78ae2a07d13dc77792c65
Vikram kautlya <89295700+Vkautlya99@users.noreply.github.com>
2022-12-17 19:00:10 +0530

    Add files via upload

---
+++ build/precache-manifest.e65fb46d2c8f2a74c804f6f494a94aef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf809efc903a8bb3b240",
+    "url": "/static/css/main.88c36dfe.chunk.css"
+  },
+  {
+    "revision": "bf809efc903a8bb3b240",
+    "url": "/static/js/main.bf809efc.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "7dc9a2d1d25818d05788",
+    "url": "/static/js/runtime~main.7dc9a2d1.js"
+  },
+  {
+    "revision": "99a822e6f2760fe80501bced16af90ac",
+    "url": "/index.html"
+  }
+];
